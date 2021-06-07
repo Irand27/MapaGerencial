3de68f00b5ba499af136549dd74345dc85c3dfc4
Iran Dias <iransdias27@gmail.com>
2021-06-06 22:12:11 -0300

    graph line implementation

---
+++ lib/pages/home/charts/chart_line.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:projeto_principal/pages/home/charts/data_charts.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+class ChartsLine extends StatefulWidget {
+  const ChartsLine({Key key}) : super(key: key);
+
+  @override
+  _SyncFusionFlutterChartsLineState createState() =>
+      _SyncFusionFlutterChartsLineState();
+}
+
+class _SyncFusionFlutterChartsLineState extends State<ChartsLine> {
+  _SyncFusionFlutterChartsLineState();
+
+  @override
+  Widget build(BuildContext context) {
+    return _getDefaultLineChart();
+  }
+
+  SfCartesianChart _getDefaultLineChart() {
+    return SfCartesianChart(
+      plotAreaBorderWidth: 0,
+      title: ChartTitle(text: ''),
+      legend: Legend(
+        isVisible: false,
+        overflowMode: LegendItemOverflowMode.wrap,
+      ),
+      primaryXAxis: NumericAxis(
+        edgeLabelPlacement: EdgeLabelPlacement.shift,
+        interval: 2,
+        majorGridLines: MajorGridLines(
+          width: 0,
+        ),
+      ),
+      primaryYAxis: NumericAxis(
+        labelFormat: '{value}%',
+        axisLine: AxisLine(width: 0),
+        majorTickLines: MajorTickLines(
+          color: Colors.transparent,
+        ),
+      ),
+      series: _getDefaultLineSeries(),
+      tooltipBehavior: TooltipBehavior(
+        enable: true,
+      ),
+    );
+  }
+
+  List<LineSeries<ChartSampleData, num>> _getDefaultLineSeries() {
+    final chartData = <ChartSampleData>[
+      ChartSampleData(x: 1, y: 21, y2: 28),
+      ChartSampleData(x: 2, y: 24, y2: 44),
+      ChartSampleData(x: 3, y: 36, y2: 48),
+      ChartSampleData(x: 4, y: 38, y2: 50),
+      ChartSampleData(x: 5, y: 54, y2: 66),
+      ChartSampleData(x: 6, y: 57, y2: 78),
+      ChartSampleData(x: 7, y: 70, y2: 84)
+    ];
+
+    return <LineSeries<ChartSampleData, num>>[
+      LineSeries<ChartSampleData, num>(
+        color: Colors.blue,
+        animationDuration: 2500,
+        dataSource: chartData,
+        xValueMapper: (ChartSampleData sales, _) => sales.x,
+        yValueMapper: (ChartSampleData sales, _) => sales.y,
+        width: 2,
+        name: 'Biscoito de sal',
+        markerSettings: MarkerSettings(isVisible: true),
+      ),
+      LineSeries<ChartSampleData, num>(
+        animationDuration: 2500,
+        dataSource: chartData,
+        width: 2,
+        name: 'Refrigerantes',
+        xValueMapper: (ChartSampleData sales, _) => sales.x,
+        yValueMapper: (ChartSampleData sales, _) => sales.y2,
+        markerSettings: MarkerSettings(
+          isVisible: true,
+        ),
+      ),
+    ];
+  }
+}
--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:projeto_principal/pages/home/charts/chart_bar.dart';
-import 'package:projeto_principal/pages/home/charts/chart_bubble.dart';
+import 'package:projeto_principal/pages/home/charts/chart_line.dart';
+import 'package:projeto_principal/pages/home/charts/chart_add.dart';
 import 'package:projeto_principal/pages/home/charts/chart_pie.dart';
 import 'package:projeto_principal/pages/home/charts/data_charts.dart';
 
@@ -34,13 +34,13 @@ class HomePage extends StatelessWidget {
                   Tab(
                     child: Align(
                       alignment: Alignment.center,
-                      child: Icon(Icons.bar_chart),
+                      child: Icon(Icons.stacked_line_chart),
                     ),
                   ),
                   Tab(
                     child: Align(
                         alignment: Alignment.center,
-                        child: Icon(Icons.bubble_chart)),
+                        child: Icon(Icons.add_chart)),
                   ),
                 ]),
           ),
@@ -73,74 +73,9 @@ class HomePage extends StatelessWidget {
                 ),
               ],
             ),
+            ChartsLine(),
             ChartBubble(),
-            ChartBar(),
           ]),
         ));
-    // return Scaffold(
-    //   appBar: AppBar(),
-    //   body: Stack(
-    //     children: [
-    //       Container(
-    //         height: double.infinity,
-    //         width: double.infinity,
-    //         decoration: BoxDecoration(
-    //           gradient: LinearGradient(
-    //               begin: Alignment.topCenter,
-    //               end: Alignment.bottomCenter,
-    //               colors: [
-    //                 Color(0xFF73AEF5),
-    //                 Color(0xFF61A4F1),
-    //                 Color(0xFF478DE0),
-    //                 Color(0xFF398AE5),
-    //               ],
-    //               stops: [
-    //                 0.1,
-    //                 0.4,
-    //                 0.7,
-    //                 0.9,
-    //               ]),
-    //         ),
-    //       ),
-    // ListView.builder(
-    //     padding: const EdgeInsets.all(8),
-    //     itemCount: entries.length,
-    //     itemBuilder: (BuildContext context, int index) {
-    //       return Container(
-    //         height: MediaQuery.of(context).size.height * 0.5,
-    //         decoration: BoxDecoration(
-    //           color: Colors.redAccent,
-    //           borderRadius: BorderRadius.circular(15),
-    //         ),
-    //         margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-    //         child: Column(
-    //           crossAxisAlignment: CrossAxisAlignment.stretch,
-    //           children: [
-    //             Container(
-    //               width: double.infinity,
-    //               color: Colors.orangeAccent,
-    //               child: Padding(
-    //                 padding: const EdgeInsets.all(4.0),
-    //                 child: Text(
-    //                   'Entry ${entries[index]}',
-    //                   style: TextStyle(
-    //                     color: Colors.black,
-    //                     fontSize: 16,
-    //                     fontWeight: FontWeight.bold,
-    //                     fontFamily: 'OpenSans',
-    //                   ),
-    //                   textAlign: TextAlign.center,
-    //                 ),
-    //               ),
-    //             ),
-    //           ],
-    //         ),
-    //       );
-    //     }),
-//         ],
-//       ),
-//     );
-//   }
-// }
   }
 }
