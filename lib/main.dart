1e73ee607193a5b4deb05bda1708d70b9f138e62
Iran Dias <iransdias27@gmail.com>
2021-06-10 23:45:44 -0300

    add splash

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,56 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      home: LoginScreen(),
+      home: FutureBuilder(
+        future: Future.delayed(Duration(seconds: 3)),
+        builder: (context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return MaterialApp(home: Splash());
+          } else {
+            return MaterialApp(
+              home: LoginScreen(),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+class Splash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          height: double.infinity,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color(0xFF73AEF5),
+                  Color(0xFF61A4F1),
+                  Color(0xFF478DE0),
+                  Color(0xFF398AE5),
+                ],
+                stops: [
+                  0.1,
+                  0.4,
+                  0.7,
+                  0.9,
+                ]),
+          ),
+        ),
+        Center(
+          child: Icon(
+            Icons.addchart_sharp,
+            size: MediaQuery.of(context).size.width * 0.785,
+            color: Colors.amberAccent,
+          ),
+        ),
+      ],
     );
   }
 }
